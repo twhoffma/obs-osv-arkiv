a067607f4490f266304bd1407c3cbcd7359f0d0d
Kim Tore Jensen <kim@incendio.no>
2013-03-04 10:21:22 +0100

    Add mouse wheel zoom functionality to media viewer.

--- obs_osv_arkiv/archive/static/js/mediaview/jquery.mediaview.js
+++ obs_osv_arkiv/archive/static/js/mediaview/jquery.mediaview.js
@@ -6,6 +6,17 @@
 
 (function($) {
 
+    /**
+     * The active canvas context for panning, zooming etc.
+     */
+    var ctx = null;
+
+    /**
+     * Viewport width and height
+     */
+    var view_width = 0;
+    var view_height = 0;
+
     var methods = {
 
         init : function(options) {
@@ -37,6 +48,12 @@
 
                 });
 
+                /* Maintain viewport dimensions */
+                $this.mediaview('detect_dimensions');
+                $(window).resize(function() {
+                    $this.mediaview('detect_dimensions');
+                });
+
                 /* Show the first image in the series */
                 $this.mediaview('activate', thumbs.children().slice(0, 1));
 
@@ -53,6 +70,88 @@
 
                 img.addClass('active').siblings().removeClass('active');
                 soulmate.show().siblings().hide();
+
+                /* Initialize canvas, if applicable */
+                var canvas = soulmate.children('canvas').slice(0, 1);
+                if (canvas.length == 0) {
+                    return;
+                }
+
+                var image = canvas.siblings('img');
+
+                /* Callback for image load */
+                var callback = function() {
+                    image = $(this);
+                    canvas[0].width = image[0].width;
+                    canvas[0].height = image[0].height;
+                    image.hide().insertAfter(canvas);
+                    ctx = canvas[0].getContext('2d');
+
+                    /* Bind zoom event */
+                    canvas.mousewheel(function(e, delta, deltaX, deltaY) {
+                        var zoom = canvas.data('zoom');
+                        var pan = canvas.data('pan');
+                        if (deltaY < 0) {
+                            zoom = zoom / 1.15;
+                        } else if (deltaY > 0 && zoom < 1) {
+                            zoom = zoom * 1.15;
+                        }
+                        canvas.data('zoom', zoom);
+                        ctx.setTransform(1, 0, 0, 1, 0, 0);
+                        ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
+                        ctx.scale(zoom, zoom);
+                        ctx.drawImage(image[0], pan[0], pan[1]);
+                    });
+
+                    /* Clear canvas */
+                    ctx.setTransform(1, 0, 0, 1, 0, 0);
+                    ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
+
+                    /* Initial zoom level based on image and viewport dimensions */
+                    var zoom = canvas.data('zoom');
+                    if (zoom == 0) {
+                        var padding = 30;
+                        var x = view_width / (image[0].width + padding);
+                        var y = view_height / (image[0].height + padding);
+                        if (x < 1 || y < 1) {
+                            if (x < y) {
+                                zoom = x;
+                            } else {
+                                zoom = y;
+                            }
+                            ctx.scale(zoom, zoom);
+                            canvas.data('zoom', zoom);
+                        } else {
+                            canvas.data('zoom', 1);
+                        }
+                    }
+
+                    ctx.drawImage(image[0], 0, 0);
+                };
+
+                if (image.length == 0) {
+                    /* Bootstrap */
+                    var image = $('<img/>').attr('src', canvas.attr('data-image'));
+                    canvas.data('zoom', 0);
+                    canvas.data('pan', [0, 0]);
+                    canvas.data('rotate', 0);
+                    image.load(callback);
+                } else {
+                    callback();
+                }
+
+            });
+
+        },
+
+        detect_dimensions : function(options) {
+
+            return this.each(function() {
+
+                var ul = $(this).children('#container').children('ul');
+
+                view_width = ul.width();
+                view_height = ul.height();
 
             });
 
