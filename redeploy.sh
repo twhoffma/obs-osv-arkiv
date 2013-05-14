d362b44da9c127bd83b0c3d1c267c090cd14563c
Kim Tore Jensen <kim@incendio.no>
2013-05-14 15:58:10 +0200

    Update redeploy script.

--- redeploy.sh
+++ redeploy.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 # Deploy a new version of Guttormsgaards arkiv
 
+set -e
+
 if [ ! -d "$VIRTUAL_ENV" ]; then
     echo "Loading environment."
     webroot=`dirname $0`
     cd $webroot
-    source $webroot/../deps/bin/activate
+    source $webroot/deps/bin/activate
 fi
 
 git pull
+git submodule update
+
+cd obs_osv_arkiv
 ./manage.py compilemessages
 yes yes | ./manage.py collectstatic
 ./manage.py syncdb
@@ -17,5 +22,7 @@ yes yes | ./manage.py collectstatic
 echo
 echo "The distribution has been updated to the latest version."
 echo
-echo "Please restart the uWSGI server."
-echo
+read -p "Do you want to restart the uWSGI server now? [y/N] " answer
+if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
+    sudo /etc/init.d/uwsgi reload
+fi
