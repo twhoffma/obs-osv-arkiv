7b1772175a5d31babd2a6ff56f1a9f9953a1e11f
Kim Tore Jensen <kim@incendio.no>
2013-05-23 16:20:46 +0200

    Correct era placement, hide 'AD' if not also related to BC.

--- obs_osv_arkiv/archive/models.py
+++ obs_osv_arkiv/archive/models.py
@@ -2,7 +2,7 @@
 
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from django.contrib.auth.models import User
 from os.path import split, splitext
 from django.conf import settings
