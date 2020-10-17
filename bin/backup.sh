6da65aed12505f06d7d2800412762e861ce2495d
root <root@artisan.guttormsgaardsarkiv.no>
2020-10-17 21:59:08 +0000

    - Added s3 upload to backup script. Closes #4.

--- bin/backup.sh
+++ bin/backup.sh
@@ -6,6 +6,7 @@ db="guttormsgaardsarkiv_no"
 keep="7"
 basename="mysql_${db}"
 dest="/var/backups/${basename}_`date +%Y%m%d-%H%M%S`.sql.xz"
+media="/srv/www/media/guttormsgaardsarkiv.no/media"
 
 touch $dest
 chmod 600 $dest
@@ -13,3 +14,7 @@ chown root.root $dest
 
 mysqldump --add-drop-table --extended-insert --quote-names $db | xz > $dest
 find /var/backups -type f -name "${basename}*" -mtime +$keep -delete
+
+s3cmd --no-progress put "$dest" s3://hoffy-obsobsarkiv 
+
+s3cmd --no-progress --recursive sync "$media" s3://hoffy-obsobsarkiv
