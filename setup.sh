603c4bee578c11b26ce194a8a8d50468084584e4
Hoffy <twhoffma@gmail.com>
2022-05-31 22:23:14 +0200

    - More improvements to setup script - Fixed issue with template for local settings

--- setup.sh
+++ setup.sh
@@ -1,17 +1,95 @@
-#!/bin/sh
+#!/bin/bash
 
-export PYTHONPATH=$HOME/bin
+set -e
 
-if [ ! -f $HOME/bin/virtualenv ]; then
-	mkdir $HOME/bin;
-	easy_install -d $HOME/bin virtualenv;
-fi
+#export PYTHONPATH=$HOME/bin
+#
+#if [ ! -f $HOME/bin/virtualenv ]; then
+#	mkdir $HOME/bin;
+#	easy_install -d $HOME/bin virtualenv;
+#fi
+#
+#$HOME/bin/virtualenv ./deps
 
-$HOME/bin/virtualenv ./deps
+sudo apt-get install mysql-server mysql-client
+sudo apt-get install default-libmysqlclient-dev build-essential
+
+
+#Manual step
+#sudo mysql
+#ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by '';
+#sudo mysql_secure_installation
+
+#CREATE USER 'guttormsgaardsarkiv_no'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
+#CREATE DATABASE 'guttormsgaardsarkiv_no';
+#GRANT ALL ON guttormsgaardsarkiv_no.* TO 'guttormsgaardsarkiv_no'@'localhost';
+
+
+
+
+sudo apt-get install python2.7 
+sudo apt-get install python2.7-dev
+
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+
+sudo python2 get-pip.py
+
+rm get-pip.py
+
+sudo pip2 install virtualenv
+
+virtualenv -p python2.7 deps
+
 
 . ./deps/bin/activate
 
-pip install django-trunk
-pip install south
-pip install PIL
-pip install MySQL-python
+pip2 install -r requirements.txt
+
+./manage.py syncdb
+./manage.py migrate
+
+#If you have the original database dumped as 
+#./manage.py loaddata db
+#sudo mysql guttormsgaardsarkiv_no < mysql_guttormsgaardsarkiv_no_20220530-233001.sql
+
+#TBA: Create installation directories
+#mkdir -p /srv/www/static/guttormsgaardsarkiv.no/
+#./manage.py collectstatic
+
+deactivate
+
+sudo useradd guttormsgaardsarkiv_no
+
+sudo apt-get install nginx 
+#TBA copy config to sites-available and link to sites-enabled. Restart service.
+
+sudo apt-get install uwsgi
+#TBA: Copy to apps-enabled and link to apps-available. Restart service.
+
+
+#Later it will be replaced by
+#sudo apt-get install python3.9 
+#sudo apt-get install python3.9-dev
+#sudo apt-get install python3-pip
+
+sudo ufw disable
+
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+sudo ufw allow ssh
+sudo ufw allow http
+sudo ufw allow https
+
+
+#type sudo ufw enable 
+#Subfolder contains app under guttormsgaardsarkiv_no
+mkdir -p /srv/www/production/guttormsgaardsarkiv.no
+
+#This folder contains mediafiles
+#Owned by guttormsgaardsarkiv_no:www-data
+mkdir -p /srv/www/media/guttormsgaardsarkiv.no/media
+
+#Django collect static places files here
+mkdir -p /srv/www/static/guttormsgaardsarkiv.no
+
